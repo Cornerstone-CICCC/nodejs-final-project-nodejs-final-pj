212885c857d2080a778c38b6de44d2ecc8e48c09
iki789 <ikhlak789@gmail.com>
2025-04-16 09:36:20 -0700

    added Message Model

---
+++ src/lib/db/models/Message.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IMessage extends Document {
+  senderId: Schema.Types.ObjectId;
+  recipientId: Schema.Types.ObjectId;
+  read: boolean;
+  text: string;
+  createdAt: Date;
+}
+
+export const messageSchema = new Schema<IMessage>(
+  {
+    senderId: { type: Schema.Types.ObjectId, required: true },
+    recipientId: { type: Schema.Types.ObjectId, required: true },
+    read: { type: Boolean, required: true, default: true },
+    text: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      transform: (_, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
+  }
+);
+
+const Message: Model<IMessage> =
+  mongoose.models.Message || mongoose.model<IMessage>("Message", messageSchema);
+
+export default Message;
