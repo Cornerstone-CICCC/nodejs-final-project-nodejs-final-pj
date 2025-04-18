f3503ecc935a0dddec1398ac3702e7ac837853e3
Tomoya <flyingfox193807@gmail.com>
2025-04-17 21:09:23 -0700

    modified hardcoded part

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -4,7 +4,8 @@ import { z } from "zod";
 
 export const userSchema = z.object({
   name: z.string().nonempty("Name is required").min(3, "Name must be 3-30 characters").max(30, "Name must be 3-30 characters"),
-  bio: z.string().max(150, "Bio must be under 150 characters").optional(),
+  bio: z.string().max(150, "Bio must be under 150 characters"),
+  userName: z.string().nonempty("Username is required").min(3, "Username must be 3-30 characters").max(30, "Username must be 3-30 characters"),
 });
 
 export type UserFormInputs = z.infer<typeof userSchema>;
