38e975939b9a359d10135180aaff59e2b6651c3a
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-04-19 23:22:12 -0700

    remove sidebar from profile page

--- src/app/(auth)/profile/[userId]/page.tsx
+++ src/app/(auth)/profile/[userId]/page.tsx
@@ -1,15 +1,9 @@
 import UserInfo from "@/components/profile/UserInfo";
-import Sidebar from "@/components/sidebar/Sidebar";
 
 const UserProfilePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6 md:grid md:grid-cols-3">
-      <div className="md:col-span-1 md:px-8">
-        <Sidebar />
-      </div>
-      <div className="md:col-span-2">
-        <UserInfo />
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <UserInfo />
     </div>
   );
 };
