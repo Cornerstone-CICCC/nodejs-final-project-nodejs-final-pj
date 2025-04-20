4234e60a6745c9b121881a853b7e3f1df37e6758
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-04-19 23:05:45 -0700

    add: sidebar for profile, notifications page

--- src/app/(auth)/notifications/page.tsx
+++ src/app/(auth)/notifications/page.tsx
@@ -1,11 +1,17 @@
 import NotificationForm from "@/components/notification/NotificationForm";
+import Sidebar from "@/components/sidebar/Sidebar";
 
 const NotificationsPage = () => {
   return (
     <div className="flex flex-col gap-5 items-center min-h-screen bg-gray-100 p-20">
       <h1 className="text-2xl font-bold">Notifications</h1>
-      <div className="w-full max-w-lg">
-        <NotificationForm />
+      <div className="w-full max-w-lg md:grid md:grid-cols-3">
+        <div className="md:col-span-1">
+          <Sidebar />
+        </div>
+        <div className="md:col-span-2">
+          <NotificationForm />
+        </div>
       </div>
     </div>
   );
--- src/app/(auth)/profile/edit/page.tsx
+++ src/app/(auth)/profile/edit/page.tsx
@@ -1,11 +1,17 @@
 import ProfileEditForm from "@/components/profile/ProfileEditForm";
+import Sidebar from "@/components/sidebar/Sidebar";
 
 const EditProfilePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-6">Edit Profile</h1>
-      <div className="w-full max-w-lg">
-        <ProfileEditForm />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 md:grid md:grid-cols-3 p-6">
+      <div className="md:col-span-1 md:px-8">
+        <Sidebar />
+      </div>
+      <div className="md:col-span-2">
+        <h1 className="text-2xl font-bold mb-6">Edit Profile</h1>
+        <div className="w-full max-w-lg">
+          <ProfileEditForm />
+        </div>
       </div>
     </div>
   );
