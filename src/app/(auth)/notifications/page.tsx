105f7c6ad9bbe6b1f7b11388ccc68c46e8a20bee
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-04-19 23:58:02 -0700

    modify: sidebar position

--- src/app/(auth)/notifications/page.tsx
+++ src/app/(auth)/notifications/page.tsx
@@ -3,14 +3,16 @@ import Sidebar from "@/components/sidebar/Sidebar";
 
 const NotificationsPage = () => {
   return (
-    <div className="flex flex-col gap-5 items-center min-h-screen bg-gray-100 p-20">
-      <h1 className="text-2xl font-bold">Notifications</h1>
-      <div className="w-full max-w-lg md:grid md:grid-cols-3">
-        <div className="md:col-span-1">
-          <Sidebar />
-        </div>
-        <div className="md:col-span-2">
-          <NotificationForm />
+    <div className="flex flex-col gap-5 items-center min-h-screen bg-gray-100 p-20  relative">
+      <div className="w-full max-w-[358px] md:col-span-1 md:px-8 md:fixed md:left-0 md:top-0 md:pt-[165px]">
+        <Sidebar />
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold">Notifications</h1>
+        <div className="w-full max-w-lg">
+          <div className="md:col-span-2">
+            <NotificationForm />
+          </div>
         </div>
       </div>
     </div>
--- src/app/(auth)/profile/edit/page.tsx
+++ src/app/(auth)/profile/edit/page.tsx
@@ -3,8 +3,8 @@ import Sidebar from "@/components/sidebar/Sidebar";
 
 const EditProfilePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 md:grid md:grid-cols-3 p-6">
-      <div className="md:col-span-1 md:px-8">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 relative p-4">
+      <div className="w-full max-w-[388px] md:col-span-1 md:px-8 md:fixed md:left-0 md:top-0 md:pt-[220px]">
         <Sidebar />
       </div>
       <div className="md:col-span-2">
